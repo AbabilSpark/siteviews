a77b34ed56543d1a23c0c80a79c9a51915631a2c
AbabilCore <ababilspark@gmail.com>
2026-07-12 00:18:47 +0600

    chore(siteviews): bump version to 2.1.0 and update dependencies

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Add the script tag to your HTML and configure it with the attributes below:
     <p>Views: <span id="views">Loading...</span></p>
 
     <script
-      src="https://unpkg.com/siteviews@2.0.5/dist/index.js"
+      src="https://unpkg.com/siteviews@2.1.0/dist/index.js"
       type="application/javascript"
       scriptfor="siteviews"
       project-name="my-site"
--- banner.ts
+++ banner.ts
@@ -4,10 +4,10 @@
  * @author          Md Ababil Hossain (https://devababil.com)
  * @website         https://siteviews.ababilspark.com
  * @license         MIT
- * @version         2.0.7
+ * @version         2.1.0
  * @repository      https://github.com/AbabilSpark/siteviews
  * @created         2024-11-10
- * @lastModified    2026-07-11
+ * @lastModified    2026-07-12
  * ------------------------------------------------------------
  * Built with passion and simplicity in mind.
  * Lightweight, flexible, and developer-friendly.
