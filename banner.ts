88872adbe9ae64345e2912213a83cf04e1408b0b
AbabilCore <ababilspark@gmail.com>
2026-07-11 16:35:24 +0600

    chore(siteviews): update website URL, version, and last modified date in banner

--- banner.ts
+++ banner.ts
@@ -2,12 +2,12 @@
  * @packageName     siteviews
  * @description     A zero-config, plug-and-play website views counter.
  * @author          Md Ababil Hossain (https://devababil.com)
- * @website         https://siteviews.netlify.app
+ * @website         https://siteviews.ababilspark.com
  * @license         MIT
- * @version         2.0.5
- * @repository      https://github.com/DevAbabil/siteviews
+ * @version         2.0.7
+ * @repository      https://github.com/AbabilSpark/siteviews
  * @created         2024-11-10
- * @lastModified    2026-03-26
+ * @lastModified    2026-07-11
  * ------------------------------------------------------------
  * Built with passion and simplicity in mind.
  * Lightweight, flexible, and developer-friendly.
