d5082db4b5e6ff62abf8d7cdccbb85e4a612977e
AbabilCore <ababilspark@gmail.com>
2026-07-12 00:18:30 +0600

    chore(siteviews): update API and documentation URLs

--- src/shared/_constants.ts
+++ src/shared/_constants.ts
@@ -1,4 +1,5 @@
-export const SITEVIEWS_API: string = "https://siteviews.vercel.app/api/count";
-export const DOC_SOURCE: string = "https://siteviews.netlify.app";
+export const SITEVIEWS_API: string =
+  "https://siteviews-api.ababilspark.com/api/count";
+export const DOC_SOURCE: string = "https://siteviews.ababilspark.com";
 export const GEO_IP_LOOKUP_API: string = "https://json.geoiplookup.io";
 export const TOAST_DURATION: number = 6000;
